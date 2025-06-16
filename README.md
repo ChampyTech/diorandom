bc9589a1944e63ff5f12fe4e3925f7e23762b3e7
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-06-16 21:17:58 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# 🤯 DioRandom
+
+DioRandom è un generatore satirico di <b>bestemmie creative</b> e surreali.
+Ogni click ti regala un’accoppiata divina con brand, lavori, o oggetti tech completamente casuali.<br>
+Combinazioni tipo: <i>Cristo WiFi</i>, <i>Madonna PDF</i>, <i>Dio Contadino</i>.<br>
+Il nostro database attualmente contiene <b>3.213 bestemmie creative</b>.<br>
+Il contenuto è solo a scopo comico, non vuole offendere nessuna religione o credo.
+Se sei facilmente offeso, ti consigliamo di usarlo comunque.<br><br>
+
+Hai un'<b>idea</b>? Vuoi aggiungere qualcosa di geniale?
+<a href="https://github.com/ChampyTech/DioRandom/issues" target="_blank">Clicca qui e inviala!</a><br><br>
---
+++ script.js
@@ -0,0 +1,41 @@
+function generaBestemmia() {
+    const soggetti = [
+        "Dio", "Gesù", "Cristo", "Madonna", "Spirito Santo",
+        "San Pietro", "San Paolo", "San Gennaro", "San Giuseppe",
+        "Sant’Antonio", "San Francesco", "Santa Maria", "Santa Chiara",
+        "Trinità", "Papa", "Messia"
+    ];
+
+    const oggetti = [
+        "Microsoft", "Google", "Kinder", "Kinder bueno", "GPT", "AI", "PowerPoint", "Microsoft Word", "PDF",
+        "iPhone", "WiFi", "Router", "Mouse", "Webcam", "Cloud", "Bluetooth", "Auricolare",
+        "Smartwatch", "SSD", "USB-C", "Cavo HDMI", "Stampante", "Tastiera",
+        "Minecraft", "Crafting Table", "Joystick", "Meccanico", "Ingegnere", "Programmatore",
+        "Panettiere", "Bagnino", "Barista", "Corriere", "Tatuatore", "Magazziniere", "Cameriere",
+        "YouTuber", "Social Media Manager", "Forno Elettrico", "Carta Igienica", "Tram di Milano", "Boia", "Pixelato",
+        "Casse Bluetooth", "Cucchiaio Sporco", "Bidet Intelligente",
+        "Cane", "Maiale", "Asino", "Pavone", "Gatto", "Coltello", "Piatto", "Tazza", "Tram",
+        "Cucchiaio", "Letto", "Doccia", "Sedia", "Frigorifero", "Cuscino", "Armadio", "Tavolo",
+        "Zaino", "Libro", "Matita", "Cimice", "Ragno", "Formica", "Neve", "Fumo", "Nebbia",
+        "Vento", "Fulmine", "Tuono", "Fango", "Erba", "Sabbia", "Terra", "Sasso", "Cemento",
+        "Vite", "Chiodo", "Martello", "Scala", "Corda", "Specchio", "Orologio", "Quadro",
+        "Pannello", "Secchio", "Serpente", "Muro", "Porta", "Finestra", "Tappeto", "Coperta",
+        "Lavatrice", "Forno", "Bidet", "Cesso", "Lavandino", "Tastiera", "Cassa", "Minestra",
+        "Tiramisù", "Polpette", "Biscotto", "Caramella", "Crostata", "Pane", "Pizza", "Cicoria",
+        "Insegna", "Semaforo", "Panchina", "Fontana", "Campanello", "Campana", "Statua",
+        "Manichino", "Corriere", "Panettiere", "Barista", "Bagnino", "Tatuatore", "Magazziniere",
+        "Programmatore", "Vigile", "Parrucchiere", "Pescatore", "Benzinaio", "Falegname",
+        "Idraulico", "Giardiniere", "Muratore", "Camionista", "Spazzino", "Fabbro", "Contadino",
+        "Postino", "Dinosauro", "Mummia", "Fantasma", "Cavaliere", "Merda",
+        "Strega", "Vampiro", "Zombi", "Scheletro", "Alieno", "Robot", "Omino", "Bue", "Capra", "Pallone da Basket",
+        "Puffo", "Yogurt alla Fragola", "Cavatappi", "Scatola di Cartone", "Maniglia", "Bottiglia d’Acqua",
+        "Zebra", "Telecomando", "Macchina Fotografica", "Pannello Solare", "Biscottiera", "Limone",
+        "Cacciavite", "Trivella", "Palo della Luce", "Semolino", "Spray Igienizzante", "Tenda da Campeggio",
+        "Disco Volante", "Borsa Frigo", "Bancomat", "Cintura di Sicurezza", "Carrello della Spesa", "Proiettore",
+        "AutoCAD", "Terremoto", "Sigma", "Chad", "Burundi", "Atmosfera", "Pianeta", "Gigante"
+    ];
+
+    const soggetto = soggetti[Math.floor(Math.random() * soggetti.length)];
+    const oggetto = oggetti[Math.floor(Math.random() * oggetti.length)];
+    document.getElementById("bestemmia").textContent = `${soggetto} ${oggetto}`;
+}
