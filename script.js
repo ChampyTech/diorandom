ff396c4cf798d9aa42f14c573df376c58aeeed9a
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-06-18 19:04:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const oggetti = [
     "Cuscinetto per Mouse", "Pennarelli Sbiadenti", "Lente per Libro", "Gancio Appenditutto", "Pettine Anti Zanzare",
     "Supporto Anti Rollover", "Barra LED per Armadio", "Faccia da Merda", "Faccia da Culo", "Panettone", "Pandoro",
     "Chiave Inglese", "Pistone", "Cuscinetto a Sfere", "Frizione", "Albero Motore", "Rondella Dentata", "Mammut",
-    "Canaglia", "Cancar@", "Cancaros@", "Reazione Chimica", "Furgoncino dei Gelati", "Camioncino dei Gelati", "Deodorante", "Soggetto Economico", "Kane/Kagna", "Vakka", "Vatican@", "Pork@", "Strapork@", "Kretin@", "Strakretin@", "Attestato di Partecipazione", "Burrocacao alla Menta", "Erba Aromatica", "Kankar@, "Strakankar@", "Stracancar@", "Kankaros@", "Strakankaros@", "Vatikan@", "Previsione del Meteo", "Tempo Meteorologico", "Skifos@", "Straskifos@", "Guardrail", "Merdos@"
+    "Canaglia", "Cancar@", "Cancaros@", "Reazione Chimica", "Furgoncino dei Gelati", "Camioncino dei Gelati", "Deodorante", "Soggetto Economico", "Kane/Kagna", "Vakka", "Vatican@", "Pork@", "Strapork@", "Kretin@", "Strakretin@", "Attestato di Partecipazione", "Burrocacao alla Menta", "Erba Aromatica", "Kankar@", "Strakankar@", "Stracancar@", "Kankaros@", "Strakankaros@", "Vatikan@", "Previsione del Meteo", "Tempo Meteorologico", "Skifos@", "Straskifos@", "Guardrail", "Merdos@"
 ];
 
 // Genera bestemmia
