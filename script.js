f18f752dfad15c4a1d6fb661d391e40bacfa5ce9
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-06-27 23:33:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ const aggettiviMaschili = [
     "Polvere", "Cuscino Poggiapiedi", "Poggiapiedi", "Sparabestemmie", "COVID-19", "Ignorante", "Disabile", "Sottomarino",
     "Acquatico", "Pikachu", "Goku", "Aereo", "Aeroplano", "Cancello Automatico", "4 tempi", "2 tempi", "Portafiori",
     "Bicarbonato di Sodio", "Zuccherato", "Zuccherificio", "Panificio", "Cotto a Vapore", "Pizza Bianca", "nel Cesso", "Cesso",
-    "Bestia", "Mercante", "Ebete", "Smerigliatrice"
+    "Bestia", "Mercante", "Ebete", "Smerigliatrice", "Impestato"
 ];
 
 // Lista aggettivi femminili da attribuire alle divinità
@@ -292,7 +292,7 @@ const aggettiviFemminili = [
     "Polvere", "Cuscino Poggiapiedi", "Poggiapiedi", "Sparabestemmie", "COVID-19", "Ignorante", "Disabile", "Sottomarina",
     "Acquatica", "Pikachu", "Goku", "Aereo", "Aeroplano", "Cancello Automatico", "4 tempi", "2 tempi", "Portafiori",
     "Bicarbonato di Sodio", "Zuccherata", "Zuccherificio", "Panificio", "Cotta a Vapore", "Pizza Bianca", "nel Cesso", "Cesso",
-    "Bestia", "Mercante", "Ebete", "Smerigliatrice"
+    "Bestia", "Mercante", "Ebete", "Smerigliatrice", "Impestata"
 ];
 
 // Lista prefissi maschili da attribuire alle divinità
@@ -306,7 +306,7 @@ const prefissiMaschili = [
 const prefissiFemminili = [
     "Porca", "Mannaggia alla", "Maledetta", "Ladra", "Schifosa", "Boia", "Fastidiosa", "Merda", "Bastarda",
     "Cogliona", "Stronza", "Lurida", "Sporca", "Rincoglionita", "Addormentata", "Scatenata", "Lurida",
-    "Fastidiosa", "Sfulminata", "Cretina", "Cicciona", "Stramerda", "Quella vacca della", "Vacca",
+    "Fastidiosa", "Sfulminata", "Cretina", "Cicciona", "Stramerda", "Quella Vacca della", "Vacca",
     "Straporca", "Stramaiala", "Puttana", "Bestia"
 ];
 
