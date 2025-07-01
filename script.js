8bb147234127201a6894f285613a40a994f5f6fe
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-07-01 17:12:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ const aggettiviMaschili = [
     "Polvere", "Cuscino Poggiapiedi", "Poggiapiedi", "Sparabestemmie", "COVID-19", "Ignorante", "Disabile", "Sottomarino",
     "Acquatico", "Pikachu", "Goku", "Aereo", "Aeroplano", "Cancello Automatico", "4 tempi", "2 tempi", "Portafiori",
     "Bicarbonato di Sodio", "Zuccherato", "Zuccherificio", "Panificio", "Cotto a Vapore", "Pizza Bianca", "nel Cesso", "Cesso",
-    "Bestia", "Mercante", "Ebete", "Smerigliatrice", "Impestato", "Stupido", "Cagnolino", "Marcio",
+    "Bestia", "Mercante", "Ebete", "Smerigliatrice", "Impestato", "Stupido", "Cagnolino", "Marcio", "Porcello",
 ];
 
 // Lista aggettivi femminili da attribuire alle divinità
@@ -292,7 +292,7 @@ const aggettiviFemminili = [
     "Polvere", "Cuscino Poggiapiedi", "Poggiapiedi", "Sparabestemmie", "COVID-19", "Ignorante", "Disabile", "Sottomarina",
     "Acquatica", "Pikachu", "Goku", "Aereo", "Aeroplano", "Cancello Automatico", "4 tempi", "2 tempi", "Portafiori",
     "Bicarbonato di Sodio", "Zuccherata", "Zuccherificio", "Panificio", "Cotta a Vapore", "Pizza Bianca", "nel Cesso", "Cesso",
-    "Bestia", "Mercante", "Ebete", "Smerigliatrice", "Impestata", "Stupida", "Cagnolina", "Marcia",
+    "Bestia", "Mercante", "Ebete", "Smerigliatrice", "Impestata", "Stupida", "Cagnolina", "Marcia", "Porcella",
 ];
 
 // Lista prefissi maschili da attribuire alle divinità
@@ -300,6 +300,7 @@ const prefissiMaschili = [
     "Porco", "Mannaggia a", "Maledetto", "Ladro", "Schifoso", "Boia", "Fastidioso", "Merda", "Bastardo",
     "Coglione", "Stronzo", "Lurido", "Sporco", "Rincoglionito", "Addormentato", "Scatenato", "Lurido",
     "Fastidioso", "Sfulminato", "Cretino", "Ciccione", "Stramerda", "Straporco", "Stramaiale", "Bestia",
+    "Porcello",
 ];
 
 // Lista prefissi femminili da attribuire alle divinità
@@ -307,7 +308,7 @@ const prefissiFemminili = [
     "Porca", "Mannaggia alla", "Maledetta", "Ladra", "Schifosa", "Boia", "Fastidiosa", "Merda", "Bastarda",
     "Cogliona", "Stronza", "Lurida", "Sporca", "Rincoglionita", "Addormentata", "Scatenata", "Lurida",
     "Fastidiosa", "Sfulminata", "Cretina", "Cicciona", "Stramerda", "Quella Vacca della", "Vacca",
-    "Straporca", "Stramaiala", "Puttana", "Bestia",
+    "Straporca", "Stramaiala", "Puttana", "Bestia", "Porcella",
 ];
 
 // Genera bestemmia
